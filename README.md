07e156dd3f80c04ccdb7dfd7d469f2dc0874faa8
Nikolai Rodionov <allanger@zohomail.com>
2023-03-21 10:38:34 +0100

    feat: Better configuration and description

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 # What's it about?
 It's just a tool to make downloading binaries for different platforms easier. 
+
 ## For example
 If you want to build a docker image, but you want to make it available on different platforms. But your tool needs other tools as dependencies, e.g. `helm`.
 To install helm on Alpine you need to use curl, wget, or something. You need to choose a version, an operating system, and an architecture. For me, it was obvious that you must be able to use `uname -m`...
@@ -32,15 +33,15 @@ Prebuilt binaries exist for **Linux x86_64** and **MacOS arm64** and **x86_64**
 
 Don't forget to add the binary to $PATH
 ```
-$ curl https://raw.githubusercontent.com/allanger/clever-install/main/scripts/download_dudo.sh | bash
-$ dudo -h
+$ curl https://raw.githubusercontent.com/allanger/dumb-downloader/main/scripts/download_dudo.sh | bash
+$ dudo --help
 ```
 ### Docker
 
 You can use the `latest` or a `tagged` docker image
 ```
-$ docker pull ghcr.io/allanger/clever-install:latest
-$ docker run ghcr.io/allanger/clever-install:latest dudo -h
+$ docker pull ghcr.io/allanger/dumb-downloader:latest
+$ docker run ghcr.io/allanger/dumb-downloader:latest dudo -h
 ```
 
 ### Build from source
@@ -48,7 +49,67 @@ $ docker run ghcr.io/allanger/clever-install:latest dudo -h
 ```
 $ cargo build --release
 ``` 
-2. Run `gum help`
+2. Run `dudo --help`
 
 # How to use?
-To be done
+## Custom configurations
+
+In case the default config is not doing the trick for you, you can pass a custom configuration, for example, you need to download a package "package-linux-amd64_x86_64_intel_v1.0.3" and this kind of name for an architecture is not supported by the `dudo`, then you can create a config file like 
+```yaml
+# config-example.yaml
+---
+---
+os:
+  macos:
+    - macos
+    - darwin
+    - mac
+    - apple
+  linux:
+    - linux
+  windows:
+    - windows
+  freebsd:
+    - freebsd
+arch:
+  x86_64:
+    - x86_64
+    - amd64
+    - amd
+    - intel
+    - amd64_x86_64_intel
+  aarch64:
+    - aarch64
+    - arm64
+    - m1
+
+```
+
+And execute `dudo -l "package-{{ os }}-{{ arch }}-{{ version}}" -p v1.0.3 -d /tmp/package` and dudo will download the package to the `/tmp/package` then, 
+
+## Dockerfile
+
+The initial intetion for developing this was to use it for writing multi-architecture Dockerfiles for my another projects. I needed to download `helm` and `helmfile` for `arm64` and `amd64`. And I couldn't come up with good simple script for settings environment variables that would point to the the correct url, because `uname -m` wasn't giving me results that I would need. I was thinkg about writing a script to create some kind of map for different architectures, but then I thought that is was already not the first time I was having that problem and I decided to come up with a tool. And here is example, how one could use it in a `Dockerfile`
+
+```DOCKERFILE
+ARG BASE_VERSION=latest
+FROM ghcr.io/allanger/dumb-downloader as builder
+RUN apt-get update -y && apt-get install tar -y
+ARG HELM_VERSION=v3.10.3
+ARG HELMFILE_VERSION=0.151.0
+ENV RUST_LOG=info
+RUN dudo -l "https://github.com/helmfile/helmfile/releases/download/v{{ version }}/helmfile_{{ version }}_{{ os }}_{{ arch }}.tar.gz" -i /tmp/helmfile.tar.gz -p $HELMFILE_VERSION
+RUN dudo -l "https://get.helm.sh/helm-{{ version }}-{{ os }}-{{ arch }}.tar.gz" -i /tmp/helm.tar.gz -p $HELM_VERSION
+RUN tar -xf /tmp/helm.tar.gz  -C /tmp && rm -f /tmp/helm.tar.gz 
+RUN tar -xf /tmp/helmfile.tar.gz  -C /tmp && rm -f /tmp/helmfile.tar.gz 
+RUN mkdir /out && for bin in `find /tmp | grep helm`; do cp $bin /out/; done
+RUN chmod +x /out/helm
+RUN chmod +x /out/helmfile
+
+FROM ghcr.io/allanger/check-da-helm-base:${BASE_VERSION} 
+COPY --from=builder /out/ /usr/bin
+RUN apk update --no-cache && apk add --no-cache jq bash
+ENTRYPOINT ["cdh"]
+```
+
+In the builder it is downloading dependencies that are needed in my final docker image.
--- src/main.rs
+++ src/main.rs
@@ -5,64 +5,251 @@ use log::{error, info};
 use serde::{Deserialize, Serialize};
 
 use std::{
+    collections::HashMap,
     env::consts::{ARCH, OS},
-    fs::File,
-    io,
+    fmt::Display,
+    fs::{File, OpenOptions},
+    io::{self},
     process::exit,
 };
+
+type Result<T> = std::result::Result<T, DudoError>;
+#[derive(Debug)]
+enum DudoError {
+    IoError(io::Error),
+    SerdeYamlError(serde_yaml::Error),
+}
+impl From<io::Error> for DudoError {
+    fn from(error: io::Error) -> Self {
+        DudoError::IoError(error)
+    }
+}
+
+impl From<serde_yaml::Error> for DudoError {
+    fn from(error: serde_yaml::Error) -> Self {
+        DudoError::SerdeYamlError(error)
+    }
+}
+
+impl Display for DudoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DudoError::SerdeYamlError(err) => write!(f, "{}", err),
+            DudoError::IoError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+static CONFIG: &str = "
+---
+os:
+  macos:
+    - macos
+    - darwin
+    - mac
+    - apple
+  linux:
+    - linux
+  windows:
+    - windows
+  freebsd:
+    - freebsd
+arch:
+  x86_64:
+    - x86_64
+    - amd64
+    - amd
+    - intel
+  aarch64:
+    - aarch64
+    - arm64
+    - m1
+";
 
 /// Maybe not that clever, but at least not dumb. Download binaries for defferent architectures easier
 #[derive(Parser)]
 #[clap(author = "allanger <allanger@zohomail.com>", version, about, long_about = None, arg_required_else_help(true))]
 struct Args {
     /// A templated link for downloading
-    #[clap(short, long, env = "CLIN_LINK")]
+    #[clap(short, long, env = "DUDO_LINK_TEMPLATE")]
     link_template: String,
     /// Version that you want to download
-    #[clap(short, long, env = "CLIN_VERSION")]
+    #[clap(short, long, env = "DUDO_PACKAGE_VERSION")]
     package_version: String,
     /// Path to download
-    #[clap(short, long, env = "CLIN_PATH")]
-    install_path: String,
+    #[clap(short, long, env = "DUDO_DOWNLOADPATH")]
+    download_path: String,
+    /// Path to dudo config file
+    #[clap(short, long, default_value = "", env = "DUDO_CONFIG")]
+    config: String,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
-struct Values {
+struct SystemValues {
     version: String,
     os: String,
     arch: String,
 }
 
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct Config {
+    os: HashMap<String, Vec<String>>,
+    arch: HashMap<String, Vec<String>>,
+}
+
 fn main() {
+    // Initial steps
     env_logger::init();
     let args = Args::parse();
-    let mut reg = Handlebars::new();
-    reg.register_template_string("download_link", args.link_template)
-        .unwrap();
 
-    let archs: Vec<String> = match ARCH {
-        "x86_64" => vec!["x86_64".to_string(), "amd64".to_string()],
-        "aarch64" => vec!["aarch64".to_string(), "arm64".to_string()],
-        _ => {
-            error!("Unknown architecture");
+    // Register download url template
+    let mut reg = Handlebars::new();
+    match reg.register_template_string("download_link", args.link_template) {
+        Ok(_) => info!("Your template is successfully registered"),
+        Err(err) => error!("{}", err),
+    };
+
+    // Set system aliases
+    let config = match parse_config(args.config) {
+        Ok(config) => config,
+        Err(err) => {
+            error!("{}", err);
             exit(1);
         }
     };
 
-    for arch in archs {
-        let version = args.package_version.clone();
-        let os = OS.to_string();
-        let values = Values { arch, os, version };
+    info!("Running on {} {}", OS, ARCH);
+    let oss = config.os.get(&OS.clone().to_string()).unwrap();
+    let archs = config.arch.get(&ARCH.clone().to_string()).unwrap();
 
-        let link = reg.render("download_link", &values).unwrap();
-        info!("Trying to download from {}", link.clone());
-        let mut resp = reqwest::blocking::get(link).unwrap();
-        if resp.status() == StatusCode::OK {
-            info!("Response is 200, I'll try to download");
-            let mut out = File::create(args.install_path).expect("failed to create file");
-            io::copy(&mut resp, &mut out).expect("failed to copy content");
-            break;
+    for arch in archs {
+        for os in oss {
+            let version = args.package_version.clone();
+            let values = SystemValues {
+                arch: arch.clone(),
+                os: os.clone(),
+                version,
+            };
+
+            let link = reg.render("download_link", &values).unwrap();
+            info!("Trying to download from {}", link.clone());
+            let mut resp = reqwest::blocking::get(link).unwrap();
+            if resp.status() == StatusCode::OK {
+                info!("Response is 200, I'll try to download");
+                let mut out =
+                    File::create(args.download_path.clone()).expect("failed to create file");
+                io::copy(&mut resp, &mut out).expect("failed to copy content");
+                exit(0);
+            }
+            info!("Will try another name for arch, because response is not 200");
         }
-        info!("Will try another name for arch, because response is not 200");
+    }
+}
+
+fn parse_config(config_path: String) -> Result<Config> {
+    let config_res: std::result::Result<Config, _>;
+    if config_path.is_empty() {
+        config_res = serde_yaml::from_str(CONFIG);
+    } else {
+        let f = OpenOptions::new().write(false).read(true).open(config_path);
+        let f = match f {
+            Ok(file) => file,
+            Err(err) => {
+                return Err(err.into());
+            }
+        };
+        config_res = serde_yaml::from_reader(f);
+    }
+    match config_res {
+        Ok(config) => Ok(config),
+        Err(err) => Err(err.into()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_config;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn parse_config_default() {
+        let config = parse_config("".to_owned()).unwrap();
+        assert_eq!(
+            config.os.get("linux").unwrap().clone(),
+            vec!["linux".to_string()]
+        );
+        assert_eq!(
+            config.os.get("windows").unwrap().clone(),
+            vec!["windows".to_string()]
+        );
+        assert_eq!(
+            config.os.get("macos").unwrap().clone(),
+            vec![
+                "macos".to_string(),
+                "darwin".to_string(),
+                "mac".to_string(),
+                "apple".to_string(),
+            ]
+        );
+        assert_eq!(
+            config.arch.get("x86_64").unwrap().clone(),
+            vec![
+                "x86_64".to_string(),
+                "amd64".to_string(),
+                "amd".to_string(),
+                "intel".to_string(),
+            ]
+        );
+        assert_eq!(
+            config.arch.get("aarch64").unwrap().clone(),
+            vec!["aarch64".to_string(), "arm64".to_string(), "m1".to_string(),]
+        )
+    }
+
+    #[test]
+    fn parse_config_custom() {
+        let config = "
+---
+os:
+  macos:
+    - macos
+  linux:
+    - linux
+  windows:
+    - windows
+  freebsd:
+    - freebsd
+arch:
+  x86_64:
+    - x86_64
+  aarch64:
+    - aarch64
+        ";
+        let mut file = NamedTempFile::new().unwrap();
+        writeln!(file, "{}", config).unwrap();
+        let path = file.into_temp_path();
+        // It's looking damn not right
+        let config = parse_config(path.to_str().unwrap().clone().to_string()).unwrap();
+        assert_eq!(
+            config.os.get("linux").unwrap().clone(),
+            vec!["linux".to_string()]
+        );
+        assert_eq!(
+            config.os.get("windows").unwrap().clone(),
+            vec!["windows".to_string()]
+        );
+        assert_eq!(
+            config.os.get("macos").unwrap().clone(),
+            vec!["macos".to_string()]
+        );
+        assert_eq!(
+            config.arch.get("x86_64").unwrap().clone(),
+            vec!["x86_64".to_string()]
+        );
+        assert_eq!(
+            config.arch.get("aarch64").unwrap().clone(),
+            vec!["aarch64".to_string()]
+        )
     }
 }
